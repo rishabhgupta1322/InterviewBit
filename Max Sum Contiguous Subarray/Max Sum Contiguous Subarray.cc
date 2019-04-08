22c1db0a874d6fa0b698af715d1018d18c1ff762
Rishabh Gupta <rishabhgupta1322@gmail.com>
2019-04-08 21:57:38 +0530

    Create Max Sum Contiguous Subarray.cc

---
+++ Max Sum Contiguous Subarray/Max Sum Contiguous Subarray.cc
@@ -0,0 +1,20 @@
+int Solution::maxSubArray(const vector<int> &A) {
+    int i, pos, max=A[0], curr_max;
+    for(i=0;i<A.size();i++)
+        if(A[i]>max){ 
+            max = A[i]; 
+            pos = i;
+        }
+    if(max <= 0){
+        return max;
+    }
+    for(i=0;i<A.size();i++){
+        curr_max += A[i];
+        if(max < curr_max){
+            max = curr_max;
+            pos = i;
+        }
+        if(curr_max < 0) curr_max = 0;
+    }
+    return max;
+}
