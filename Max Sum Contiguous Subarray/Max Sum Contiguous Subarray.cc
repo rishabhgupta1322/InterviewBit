f482b55817f3955fc5a063ccf7676aa0a23385af
Prakhar Agrawal <prakhargvp@gmail.com>
2019-04-29 12:09:52 +0530

    Update Max Sum Contiguous Subarray.cc

--- Max Sum Contiguous Subarray/Max Sum Contiguous Subarray.cc
+++ Max Sum Contiguous Subarray/Max Sum Contiguous Subarray.cc
@@ -1,14 +1,9 @@
 int Solution::maxSubArray(const vector<int> &A) {
     int max, curr_max = 0;
-    // Find maximun element
-    max = *max_element(A.begin(), A.end()); // c++11
-    // if max is less than zero return it
-    if ( max < 1 ) return max;
-    // if max is not negative number
     for ( auto i = A.begin(); i != A.end(); ++i )
     {
         curr_max += *i;
-        if ( max < curr_max ) max = curr_max;
+        if ( i == A.begin() || max < curr_max ) max = curr_max;
         if ( curr_max < 0 ) curr_max = 0;
     }
     return max;
